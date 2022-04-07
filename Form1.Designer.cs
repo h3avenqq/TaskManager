27c807148aae6377906eebefd43b5a8bff6566d3
Maxim Shamrev <shmarev082@gmail.com>
2022-04-07 21:32:54 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BtnRename = new System.Windows.Forms.Button();
+            this.BtnCollapse = new System.Windows.Forms.Button();
+            this.BtnKill = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(12, 7);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.BtnRefresh.TabIndex = 1;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(643, 402);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // BtnRename
+            // 
+            this.BtnRename.Location = new System.Drawing.Point(94, 7);
+            this.BtnRename.Name = "BtnRename";
+            this.BtnRename.Size = new System.Drawing.Size(75, 23);
+            this.BtnRename.TabIndex = 3;
+            this.BtnRename.Text = "Rename";
+            this.BtnRename.UseVisualStyleBackColor = true;
+            this.BtnRename.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnCollapse
+            // 
+            this.BtnCollapse.Location = new System.Drawing.Point(176, 7);
+            this.BtnCollapse.Name = "BtnCollapse";
+            this.BtnCollapse.Size = new System.Drawing.Size(75, 23);
+            this.BtnCollapse.TabIndex = 4;
+            this.BtnCollapse.Text = "Collapse";
+            this.BtnCollapse.UseVisualStyleBackColor = true;
+            this.BtnCollapse.Click += new System.EventHandler(this.BtnCollapse_Click);
+            // 
+            // BtnKill
+            // 
+            this.BtnKill.BackColor = System.Drawing.SystemColors.Control;
+            this.BtnKill.Location = new System.Drawing.Point(258, 7);
+            this.BtnKill.Name = "BtnKill";
+            this.BtnKill.Size = new System.Drawing.Size(37, 23);
+            this.BtnKill.TabIndex = 5;
+            this.BtnKill.Text = "Kill";
+            this.BtnKill.UseVisualStyleBackColor = false;
+            this.BtnKill.Click += new System.EventHandler(this.BtnKill_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 450);
+            this.Controls.Add(this.BtnKill);
+            this.Controls.Add(this.BtnCollapse);
+            this.Controls.Add(this.BtnRename);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BtnRefresh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BtnRename;
+        private System.Windows.Forms.Button BtnCollapse;
+        private System.Windows.Forms.Button BtnKill;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Runtime.InteropServices;
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+
+        List<Process> processesList = new List<Process>();
+
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+            Restore = 9, ShowDefault = 10, ForceMinimized = 11
+        };
+
+        private struct Windowplacement
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public System.Drawing.Point ptMinPosition;
+            public System.Drawing.Point ptMaxPosition;
+            public System.Drawing.Rectangle rcNormalPosition;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshProcs()
+        {
+            dataGridView1.Rows.Clear();
+            processesList.Clear();
+            int k = 0;
+            var allProcess = from pr in Process.GetProcesses(".")
+                             orderby pr.Id
+                             select pr;
+            foreach (var proc in allProcess)
+            {
+                if (proc.MainWindowHandle != IntPtr.Zero && !String.IsNullOrEmpty(proc.MainWindowTitle.ToString()))
+                {
+                    string[] arr = { "" + proc.Id, "" + proc.ProcessName, "" + proc.MainWindowTitle,
+                    "" + proc.WorkingSet64 /1024/1024 + " MB", "" + proc.VirtualMemorySize64 /1024/1024 + " MB", ""+ proc.BasePriority};
+                    dataGridView1.Rows.Add(arr);
+                    processesList.Add(proc);
+                    k++;
+                }
+            }
+            this.Text = "Number of opened windows: " + k.ToString() + " Number of all processes " + allProcess.Count<Process>().ToString();
+        }
+
+        private void InitGrid()
+        {
+            dataGridView1.Columns.Add("Id", "Id");
+            dataGridView1.Columns.Add("ProcessName", "Process Name");
+            dataGridView1.Columns.Add("WindowName", "Window Name");
+            dataGridView1.Columns.Add("WorkingSet", "Working Set");
+            dataGridView1.Columns.Add("VirtualMemory", "Virtual Memory");
+            dataGridView1.Columns.Add("BasePriority", "Base Priority");
+        }
+
+        private static void KillProcessAndChildren(int pid)
+        {
+            if (pid == 0)
+            {
+                return;
+            }
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+                    ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (ArgumentException)
+            {}
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            RefreshProcs();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitGrid();
+            RefreshProcs();
+        }
+
+        private IntPtr FindhWnd()
+        {
+            string name = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+
+            foreach (var proc in processesList)
+            {
+                if (proc.ProcessName.Contains(name))
+                {
+                    return proc.MainWindowHandle;
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        //переименовывание
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr hWnd = FindhWnd();
+            string name = Interaction.InputBox("Rename window");
+            SetWindowText(hWnd, name);
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowText(IntPtr hWnd, string text);
+
+        //сворачивание/разворачивание
+        private void BtnCollapse_Click(object sender, EventArgs e)
+        {
+            IntPtr wdwIntPtr = FindhWnd();
+
+            Windowplacement placement = new Windowplacement();
+            GetWindowPlacement(wdwIntPtr, ref placement);
+
+            if (placement.showCmd == 2)
+            {
+                ShowWindow(wdwIntPtr, ShowWindowEnum.Restore);
+            }
+            else
+            {
+                ShowWindow(wdwIntPtr,ShowWindowEnum.ShowMinimized);
+            }
+
+            SetForegroundWindow(wdwIntPtr);
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowPlacement(IntPtr hWnd, ref Windowplacement lpwndpl);
+
+        private void BtnKill_Click(object sender, EventArgs e)
+        {
+            int parentId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value.ToString());
+            KillProcessAndChildren(parentId);
+        }
+    }
+}
+
